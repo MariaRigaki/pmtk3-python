3b8cacf21c3b68cce45e024eb2611a731989c173
Maria Rigaki <mrigaki@gmail.com>
2013-08-17 20:14:01 +0200

    easiest way

--- demos/bernoulliEntropyFig.py
+++ demos/bernoulliEntropyFig.py
@@ -4,8 +4,8 @@ import numpy as np
 
 x = np.arange(0.0001, 1.0, 0.0001)
 
-#H = -(x.*log2(x) + (1-x).*log2(1-x))
-H = [-(i*np.log2(i) + (1-i)*np.log2(1-i)) for i in x]
+H = -(x*np.log2(x) + (1-x)*np.log2(1-x))
+#H = [-(i*np.log2(i) + (1-i)*np.log2(1-i)) for i in x]
 
 plt.plot(x, H)
 plt.xlabel('p(X = 1)')
